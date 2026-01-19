d35378ab4e51ef1174626148d960f0f233a7f64e
Manolis Zaf <manos_zaf@hotmail.com>
2026-01-19 22:07:37 +0100

    error handling in asynchronous

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,63 +1,77 @@
-const User = require("../models/user.model");
+const User = require('../models/user.model');
 //class User is here available
 const authUtil = require('../util/authentication');
 
 function getSignup(req, res) {
-  res.render("customer/auth/signup");
+  res.render('customer/auth/signup');
 }
 
-async function signup(req, res) {
+async function signup(req, res, next) {
   const user = new User(
     req.body.email,
     req.body.password,
     req.body.fullname,
     req.body.street,
     req.body.postal,
-    req.body.city
-  ); 
+    req.body.city,
+  );
   //we create a concrete instance of a User blueprint
-  await user.signup(); /* We call the signup method, which is defined
+  try {
+    await user.signup(); /* We call the signup method, which is defined
   to store that user in the database.
   the signup method returns a promise */
+  } catch (error) {
+    next(error);
+    /*When we pass an error to next and we execute next with that error
+    the default error handling middleware will become active.
+    */
+    return;
+  }
 
   res.redirect('/login');
-  
 }
 
 function getLogin(req, res) {
   res.render('customer/auth/login');
 }
 
-async function login(req, res){
+async function login(req, res, next) {
   const user = new User(req.body.email, req.body.password);
-  const existingUser = await user.getUserWithSameEmail();
-  
-  if(!existingUser){
-    res.redirect('/login');
-    return;
-  }
-  const passwordIsCorrect = await user.hasMatchingPassword(existingUser.password);
-
-  if(!passwordIsCorrect){
-    res.redirect('/login');
+  let existingUser; //We define here the variable, so that it is available in the try block
+  try {
+    existingUser = await user.getUserWithSameEmail();
+  } catch (error) {
+    next(error);
     return;
   }
 
-authUtil.createUserSession(req, existingUser, function(){
-  res.redirect('/');//This will be executed, only when the session is saved
-});
+  if (!existingUser) {
+    res.redirect('/login');
+    return;
+  }
+  const passwordIsCorrect = await user.hasMatchingPassword(
+    existingUser.password,
+  );
+
+  if (!passwordIsCorrect) {
+    res.redirect('/login');
+    return;
+  }
+
+  authUtil.createUserSession(req, existingUser, function () {
+    res.redirect('/'); //This will be executed, only when the session is saved
+  });
 }
 
-function logout(req, res){
+function logout(req, res) {
   authUtil.destroyUserAuthSession(req);
   res.redirect('/login');
 }
-
 
 module.exports = {
   getSignup: getSignup,
   getLogin: getLogin,
   signup: signup,
   login: login,
-  logout: logout
+  logout: logout,
 };
