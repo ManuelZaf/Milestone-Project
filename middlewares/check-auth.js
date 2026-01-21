d25b203fe5e91cf8ffebdd384f602367ca09906a
Manolis Zaf <manos_zaf@hotmail.com>
2026-01-21 21:25:48 +0100

    admin authorization && JS,HTML nav bar

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -1,12 +1,14 @@
-function checkAuthStatus(req, res, next){
-    const uid = req.session.uid;
-    //in authentication.js we create the session and we store the uid
-    if(!uid){ //we check if the user exists, if not the function ends and the programm continues to the next middleware
-        return next();
-    }
-    res.locals.uid = uid;
-    res.locals.isAuth = true;
-    next();
+function checkAuthStatus(req, res, next) {
+  const uid = req.session.uid;
+  //in authentication.js we create the session and we store the uid
+  if (!uid) {
+    //we check if the user exists, if not the function ends and the programm continues to the next middleware
+    return next();
+  }
+  res.locals.uid = uid;
+  res.locals.isAuth = true;
+  res.locals.isAdmin = req.session.isAdmin;
+  next();
 }
 
 module.exports = checkAuthStatus;
--- util/authentication.js
+++ util/authentication.js
@@ -1,17 +1,18 @@
-function createUserSession(req, user, action){
-    /*request object, because we need to access the session
+function createUserSession(req, user, action) {
+  /*request object, because we need to access the session
     the user that we created, with all the user data
     action should be executed once the session is updated (redirect for example) */
-req.session.uid = user._id.toString(); //the _id from the database
-//that is available because of the express-session package
-req.session.save(action); //it runs the anonymous function in login authUtil in auth.controller.js
+  req.session.uid = user._id.toString(); //the _id from the database
+  //that is available because of the express-session package
+  req.session.isAdmin = user.isAdmin; //store the isAdmin information in the session
+  req.session.save(action); //it runs the anonymous function in login authUtil in auth.controller.js
 }
 
-function destroyUserAuthSession(req){
-    req.session.uid = null;
+function destroyUserAuthSession(req) {
+  req.session.uid = null;
 }
 
 module.exports = {
-    createUserSession: createUserSession,
-    destroyUserAuthSession: destroyUserAuthSession
+  createUserSession: createUserSession,
+  destroyUserAuthSession: destroyUserAuthSession,
 };
